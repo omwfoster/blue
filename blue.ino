e31cec01fd51787989f2b8a37cc427694e00bf9e
omwfoster <omwfoster@gmail.com>
2017-09-23 17:21:33 +0100

    running cylon routine, and parsing data.

--- blue.ino
+++ blue.ino
@@ -12,33 +12,34 @@ CRGB leds[NUM_LEDS];
 void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } } 
 
 int read_line(char * buffer, int bufsize) {
-clear_input_buffer(buffer, 20);
-for (int index = 0; index < bufsize; index++) {
-// Wait until characters are available
-while (Serial.available() == 0) {
-}
+    clear_input_buffer(buffer, 20);
+    for (int index = 0; index < bufsize; index++) {
+        // Wait until characters are available
+            while (Serial.available() == 0) {
+                cylon();
+            }
 
-char ch = Serial.read(); // read next character
-Serial.print(ch); // echo it back: useful with the serial monitor (optional)
+            char ch = Serial.read(); // read next character
+            Serial.print(ch); // echo it back: useful with the serial monitor (optional)
 
-if (ch == '\n') {
-buffer[index] = '\0'; // end of line reached: null terminate string
-return index; // success: return length of string (zero if string is empty)
-}
-//test
-buffer[index] = ch; // Append character to buffer
-}
+            if (ch == '\n') {
+            buffer[index] = '\0'; // end of line reached: null terminate string
+            return index; // success: return length of string (zero if string is empty)
+            }
+    //test
+    buffer[index] = ch; // Append character to buffer
+    }
 
 // Reached end of buffer, but have not seen the end-of-line yet.
 // Discard the rest of the line (safer than returning a partial line).
 
 char ch; 
 do {
-// Wait until characters are available
-while (Serial.available() == 0) {
-}
-ch = Serial.read(); // read next character (and discard it)
-Serial.print(ch); // echo it back
+    // Wait until characters are available
+    while (Serial.available() == 0) {
+    }
+    ch = Serial.read(); // read next character (and discard it)
+    Serial.print(ch); // echo it back
 }while (ch != '\n'); 
 
 buffer[0] = 0; // set buffer to empty string even though it should not be used
@@ -47,38 +48,39 @@ return-1; // error: return negative one to indicate the input was too long
 
 
 void setup() {
-pinMode(LED_PIN, OUTPUT); 
-Serial.begin(9600); 
-Serial.println("Welcome");
-LEDS.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS); 
-LEDS.setBrightness(84); 
+    pinMode(LED_PIN, OUTPUT); 
+    Serial.begin(9600); 
+    Serial.println("Welcome");
+    LEDS.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS); 
+    LEDS.setBrightness(84); 
 }
 
 void loop() {
-Serial.print("> "); 
+    Serial.print("> "); 
 
-// Read command
+    // Read command
 
-char line[LINE_BUFFER_SIZE]; 
-clear_input_buffer[line,LINE_BUFFER_SIZE];
-if (read_line(line, sizeof(line)) < 0) {
-Serial.println("Error: line too long"); 
-return; // skip command processing and try again on next iteration of loop
-}
+    char line[LINE_BUFFER_SIZE]; 
+    //Serial.printNumber(Serial.available());
+    clear_input_buffer[line,LINE_BUFFER_SIZE];
+    if (  read_line(line, sizeof(line)) < 0) {
+        Serial.println("Error: line too long"); 
+    return; // skip command processing and try again on next iteration of loop
+    }
 
-// Process command
+    // Process command
 
-if (strcmp(line, "off") == 0) {
-digitalWrite(LED_PIN, LOW); 
-}else if (strcmp(line, "on") == 0) {
-digitalWrite(LED_PIN, HIGH); 
-}else if (strcmp(line, "") == 0) {
-// Empty line: no command
-}else {
-Serial.print("Error: unknown command: \"");
-    Serial.print(line);
-    Serial.println("\" (available commands: \"off\", \"on\")"); 
-}
+    if (strcmp(line, "off") == 0) {
+        digitalWrite(LED_PIN, LOW); 
+    }else if (strcmp(line, "on") == 0) {
+        digitalWrite(LED_PIN, HIGH); 
+    }else if (strcmp(line, "") == 0) {
+    // Empty line: no command
+    }else {
+        Serial.print("Error: unknown command: \"");
+        Serial.print(line);
+        Serial.println("\" (available commands: \"off\", \"on\")"); 
+    }
 }
 
 int  clear_input_buffer(char * local_buffer, int buf_len) {
@@ -94,7 +96,7 @@ int print_buffer(char * local_buffer, int buf_len) {
     
     }
 
-    void cylon() {  
+void cylon() {  
         static uint8_t hue = 0; 
         Serial.print("x"); 
         // First slide the led in one direction 
@@ -123,6 +125,6 @@ int print_buffer(char * local_buffer, int buf_len) {
             // Wait a little bit before we loop around and do it again 
             delay(10); 
         } 
-    } 
+} 
 
 
