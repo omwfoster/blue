f298ca0976b71bc9c3ec174ce5bbadef91142538
omwfoster <omwfoster@gmail.com>
2017-09-23 19:25:56 +0100

    added hashmap

--- blue.ino
+++ blue.ino
@@ -1,13 +1,24 @@
-#include "FastLED.h"
+
+#include <Wire.h>
+#include <HashMap.h>
+#include "Countable.h"
+#include <FastLED.h>
 #define NUM_LEDS 64 
 #define DATA_PIN 7
 #define CLOCK_PIN 13
+#define HASH_SIZE 26
 
 
 
 const int LED_PIN = 13; 
 const int LINE_BUFFER_SIZE = 80; // max line length is one less than this
 CRGB leds[NUM_LEDS]; 
+
+
+
+HashType<char const *, int> hashRawArray[HASH_SIZE];
+HashMap<char const *, int> strangeLed = HashMap<char const *, int>(hashRawArray, HASH_SIZE);
+
 
 void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } } 
 
@@ -53,6 +64,34 @@ void setup() {
     Serial.println("Welcome");
     LEDS.addLeds<WS2812,DATA_PIN,RGB>(leds,NUM_LEDS); 
     LEDS.setBrightness(84); 
+
+    strangeLed[0]("a", 1);
+    strangeLed[1]("b", 1);
+    strangeLed[2]("c", 1);
+    strangeLed[3]("d", 1);
+    strangeLed[4]("e", 1);
+    strangeLed[5]("f", 1);
+    strangeLed[6]("g", 1);
+    strangeLed[7]("h", 1);
+    strangeLed[8]("i", 1);
+    strangeLed[9]("j", 1);
+    strangeLed[10]("k", 1);
+    strangeLed[11]("l", 1);
+    strangeLed[12]("m", 1);
+    strangeLed[13]("n", 1);
+    strangeLed[14]("o", 1);
+    strangeLed[15]("p", 1);
+    strangeLed[16]("q", 1);
+    strangeLed[17]("r", 1);
+    strangeLed[18]("s", 1);
+    strangeLed[19]("t", 1);  
+    strangeLed[20]("u", 1);
+    strangeLed[21]("v", 1);
+    strangeLed[22]("w", 1);
+    strangeLed[23]("x", 1);
+    strangeLed[24]("y", 1);
+    strangeLed[25]("z", 1);
+    strangeLed[26]("x", 1);
 }
 
 void loop() {
@@ -127,4 +166,35 @@ void cylon() {
         } 
 } 
 
+
+void strangerlite(char * buffer, int buf_size ) {  
+    static uint8_t hue = 0; 
+    Serial.print("x"); 
+    // First slide the led in one direction 
+    for(int i = 0; i < NUM_LEDS; i++) { 
+        // Set the i'th led to red  
+        leds[i] = CHSV(hue++, 255, 255); 
+        // Show the leds 
+        FastLED.show();  
+        // now that we've shown the leds, reset the i'th led to black 
+        // leds[i] = CRGB::Black; 
+        fadeall(); 
+        // Wait a little bit before we loop around and do it again 
+        delay(10); 
+    } 
+    Serial.print("x"); 
+ 
+    // Now go in the other direction.   
+    for(int i = (NUM_LEDS)-1; i >= 0; i--) { 
+        // Set the i'th led to red  
+        leds[i] = CHSV(hue++, 255, 255); 
+        // Show the leds 
+        FastLED.show(); 
+        // now that we've shown the leds, reset the i'th led to black 
+        // leds[i] = CRGB::Black; 
+        fadeall(); 
+        // Wait a little bit before we loop around and do it again 
+        delay(10); 
+    } 
+} 
 
